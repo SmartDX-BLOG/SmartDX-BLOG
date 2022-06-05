46f20662d75ba9dbda9af51fcede77bd67852af0
이채하 <chaeha93@gmail.com>
2022-06-05 20:54:57 +0900

    create usr models.py

--- smartDX/user/models.py
+++ smartDX/user/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Usr(models.Model):
+    usr_sno = models.AutoField(primary_key=True)
+    id = models.CharField(max_length=100)
+    pwd = models.CharField(max_length=100)
+    nm = models.CharField(max_length=100)
+    eml = models.CharField(max_length=100)
+    sgnup_dt = models.DateField()
+
+    def _str_(self):
+        return self.usr_sno
