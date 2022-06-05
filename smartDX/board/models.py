7f4daf61ac756a34f6cad64ad46ad720c80f14b3
ziageem <ziageem@gmail.com>
2022-06-05 21:24:26 +0900

    Init: DB settings

--- smartDX/board/models.py
+++ smartDX/board/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from user.models import Usr
 
 # Create your models here.
+class Brd(models.Model):
+    brd_sno = models.AutoField(primary_key=True)
+    usr_sno = models.ForeignKey(Usr, on_delete=models.CASCADE, db_column='usr_sno')
+    ttl = models.CharField(max_length=100)
+    cntnt = models.CharField(max_length=300)
+    dt = models.DateField()
+    clck = models.IntegerField(default=0)
+
+    def _str_(self):
+        return self.brd_sno
+
+class Fl(models.Model):
+    fl_sno = models.AutoField(primary_key=True)
+    brd_sno = models.ForeignKey(Brd, on_delete=models.CASCADE, db_column='brd_sno')
+    usr_sno = models.ForeignKey(Usr, on_delete=models.CASCADE, db_column='usr_sno')
+    orgnl_fnm = models.CharField(max_length=100)
+    fnm = models.CharField(max_length=150)
+
+    def _str_(self):
+        return self.fl_sno
--- smartDX/comment/models.py
+++ smartDX/comment/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from user.models import Usr
+from board.models import Brd
 
 # Create your models here.
+class Commnt(models.Model):
+    commnt_sno = models.AutoField(primary_key=True)
+    usr_sno = models.ForeignKey(Usr, on_delete=models.CASCADE, db_column='usr_sno')
+    contnt = models.CharField(max_length=300)
+    dt = models.DateField()
+    brd_sno = models.ForeignKey(Brd, on_delete=models.CASCADE, db_column='brd_sno')
+
+    def _str_(self):
+        return self.commnt_sno
